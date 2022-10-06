c39e2d359282a39be42759e90b007825293e452e
Sondre Melhus <42571126+SondreEMelhus@users.noreply.github.com>
2022-10-06 17:35:29 +0200

    Implemented static project banners and a home page

--- src/app/component/login/Login.jsx
+++ src/app/component/login/Login.jsx
@@ -6,18 +6,19 @@ import keycloak from "../keycloak/keycloak";
 
 //Styling
 import '../../../css/login.css'
+import ProjectBanner from "../projects/ProjectBanner";
 
 export default function Login () {
 
     return (
         <>
-            {!keycloak.authenticated &&
+            { !keycloak.authenticated &&
                 <>
                     <button className="loginButton" onClick={ keycloak.login }>Logg in</button>
                     <button className="registerButton" onClick={ keycloak.register }>Registrer</button>
                 </>
             }
-            {keycloak.authenticated && <button className="loginButton" onClick={ keycloak.logout }>Logg ut</button>}
+            { keycloak.authenticated && <button className="loginButton" onClick={ keycloak.logout }>Logg ut</button> }
         </>
     )
 }

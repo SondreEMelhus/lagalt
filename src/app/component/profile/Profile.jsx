3ca9c3bd991630798aba11e0ca6b368070350d31
Ulrik Lunde <ulrik_lunde@hotmail.com>
2022-10-06 15:19:08 +0200

    feat: profile styling

--- src/app/component/profile/Profile.jsx
+++ src/app/component/profile/Profile.jsx
@@ -4,12 +4,34 @@ import React from "react";
 //Components
 
 //Styling
+import '../../../css/profile.css'
 
 export default function Profile () {
 
     return (
         <>
-            <h1>Profile page</h1>
+            <h2 className="yourProfile">Din profil</h2>
+
+            <div>
+                <p className="skillsHeadProfile">Mine ferdigheter</p>
+                <div className="skillsFieldProfile">
+                    <div className="skillElementProfile">
+                        <p>Java</p>
+                    </div>
+                </div>
+            </div>
+            <div className="portfolioFieldProfile">
+                <p className="portfolioHeadProfile">Portfolio</p>
+                <textarea name="" id="" cols="80" rows="10" className="textAreaField"></textarea>
+            </div>
+            <div className="portfolioFieldProfile">
+                <p className="portfolioHeadProfile">Min beskrivelse</p>
+                <textarea name="" id="" cols="80" rows="10" className="textAreaField"></textarea>
+            </div>
+            <div className="statusDiv">
+                <p className="profileStatusText">Profil status:</p>
+                <button className="statusButtonProfile">Offentlig</button>
+            </div>
         </>
     )
 }

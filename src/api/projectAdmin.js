f40506507a5c357a234c3cd4eb2da233fe24421e
Ulrik Lunde <ulrik_lunde@hotmail.com>
2022-10-12 08:24:40 +0200

    feat: my projects

--- src/api/projectAdmin.js
+++ src/api/projectAdmin.js
@@ -1,9 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+
+const dispatch = useDispatch();
+
 const apiUrl = "https://lagalt-java-backend.herokuapp.com/api/v1"
 
-export const updateStatus = () => {
+export const getProjectById = async (id) => {
     try{
-
+        const response = await fetch(`${apiUrl}/projects/${id}`);
+        console.log(response);
+        if(!response.ok){
+            throw new Error("No projects found");
+        }
+        const data = await response.json();
+        return [null, data]
     }catch(error){
-        return [error.message, []];
+        return[error.message, []];
     }
 }
--- src/app/component/myProjects/myProjects.jsx
+++ src/app/component/myProjects/myProjects.jsx
@@ -1,16 +1,47 @@
 //Libraries
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 //Components
+import Navbar from "../navbar/Navbar";
+import ProjectBanner from "../projects/ProjectBanner";
+import { selectProjects } from "../redux/slices/ProjectsSlice";
+import { updateSkills } from "../redux/slices/filters/lists/SkillsSlice";
+import { updateKeywords } from "../redux/slices/filters/lists/KeywordsSlice";
+import { generateKeywordState, generateSkillsState } from "../util/StatePopulator";
 
 //Styling
 import '../../../css/profile.css'
 
 export default function MyProjects () {
 
+    const projects = useSelector(selectProjects)
+    const dispatch = useDispatch();
+
+    //Populate skills and keywords in filter
+    useEffect(() => {
+        dispatch( updateSkills( generateSkillsState(projects) ));
+        dispatch( updateKeywords( generateKeywordState(projects)));
+    }, [])
+
+    function sendToAdmin(){
+        
+    }
+
     return (
         <>
-            <p>My Projects</p>
+            <Navbar/>
+            <p>Mine prosjekter</p>
+            <div className="home-body">
+                    {projects.map((project, index) => {
+                        return (
+                            <ProjectBanner key={index + '-' + project.id} project={project}  onClick={sendToAdmin}/>
+                        )
+                    })
+
+                    }
+                </div>
+
         </>
     )
 }

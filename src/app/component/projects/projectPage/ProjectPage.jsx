b2445e26c0694b3c4fc5bbbd319660c757168321
Ulrik Lunde <ulrik_lunde@hotmail.com>
2022-10-12 12:57:13 +0200

    feat: projectPage

--- src/app/component/projects/projectPage/ProjectPage.jsx
+++ src/app/component/projects/projectPage/ProjectPage.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+//Components
+import Navbar from "../../navbar/Navbar";
+import codeSymbol from '../../../../assets/codingIcon.png'
+
+//Style
+import '../../../../css/project.css'
+
+
 export default function ProjectPage (){
 
     //Industry image - Tittel - Status - Bli med button - Administrate button
@@ -12,9 +20,21 @@ export default function ProjectPage (){
     //UpdateBoard(MessageBoard comp)
     //div/
     return(
-
         <div>
-            <p>Project page</p>
+            <Navbar/>
+            <div className="topDivProject">
+                <img src={codeSymbol} alt="" className="icon"/>
+                <h2>Prosjekt tittel</h2>
+                <div className="statusField">
+                    <p className="statusText">Startet</p>
+                </div>
+                <button className="joinButton">Bli med</button>
+                <button className="adminButton">Administrer</button>
+            </div>
+            <div className="projectInfoField">
+                <h3 className="projecSubTitle">Prosjekt tittel</h3>
+                <p className="descriptionProject">Jeg ønsker å utvikle en web app Jeg ønsker å utvikle en web app Jeg ønsker å utvikle en web app Jeg ønsker å utvikle en web app Jeg ønsker å utvikle en web app Jeg ønsker å utvikle en web app Jeg ønsker å utvikle en web</p>
+            </div>
         </div>
     )
 }

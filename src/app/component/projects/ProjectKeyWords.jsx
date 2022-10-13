7f8efc8e55e15457837904fb8427b5e3ba697f85
Ulrik Lunde <ulrik_lunde@hotmail.com>
2022-10-13 10:23:07 +0200

    feat: admin tweeks

--- src/app/component/projects/ProjectKeyWords.jsx
+++ src/app/component/projects/ProjectKeyWords.jsx
@@ -1,13 +1,36 @@
 //Libraries
-import React from "react";
+import React, { useState } from "react";
 
 //Components
 import KeyWord from '../../../assets/KeyWord.png'
 import XLetter from '../../../assets/xLetter.png'
+import { useSelector } from "react-redux";
+import { selectProject } from "../redux/slices/ProjectSlice";
 //Styling
 import '../../../css/projectKeyWords.css'
 
 export default function ProjectKeyWords () {
+
+    const project = useSelector(selectProject);
+    const [keywords, setKeywords] = useState(project.keywords)
+
+    const removeKeyword = (event) => {
+        console.log("hello")
+        const word2 = document.getElementById("image").value;
+
+        console.log(word2)
+        const newKeywords = [];
+        for(let word of keywords){
+            console.log("-------")
+            console.log(event.target.value)
+            console.log("-------")
+
+            if(word === event){
+                newKeywords.push(word);
+            }
+        }
+        console.log(newKeywords);
+    }
 
     return (
         <div className="keyWorddivAdmin">
@@ -15,9 +38,18 @@ export default function ProjectKeyWords () {
             <img src={KeyWord} alt=""  className="keyWordIconAdmin"/>
             <input type="text" class="headKeyWordAdmin" placeholder="Nøkkelord..."/>
         </div>
-        <div class="keyWordElementAdmin">
-            <p className="eachKeyWordElementAdmin">Guitar</p>
-            <img src={XLetter} alt="" className="xletterAdmin"/>
+        <div className="keywordElementsContainer">
+        {keywords.map((keyword, index) => {
+            
+            return(
+                <div class="keyWordElementAdmin" key={index + '-' + keyword}>
+            <p className="eachKeyWordElementAdmin">{keyword}</p>
+            <div >
+            <img src={XLetter} alt="ffs" className="xletterAdmin" id="image" value={index} onClick={removeKeyword}/>
+            </div>
+        </div>
+            )
+        })}
         </div>
     </div>
     )
--- src/app/component/projects/ProjectSkills.jsx
+++ src/app/component/projects/ProjectSkills.jsx
@@ -4,21 +4,34 @@ import React from "react";
 //Components
 import SkillIcon from '../../../assets/Skillicon.png'
 import XLetter from '../../../assets/xLetter.png'
+import { useSelector } from "react-redux";
+import { selectProject } from "../redux/slices/ProjectSlice";
 //Styling
 import '../../../css/projectSkills.css'
 
 export default function ProjectSkills () {
 
+    const project = useSelector(selectProject);
+    const projectSkills = project.skills;
+    console.log(projectSkills)
     return (
         <div className="skillsdivAdmin">
         <div className="topPartSkillAdmin">
-            <img src={SkillIcon} alt=""  className="skillIconAdmin"/>
+                <img src={SkillIcon} alt=""  className="skillIconAdmin"/>
             <input type="text" class="headSkillAdmin" placeholder="Ferdigheter..."/>
         </div>
-        <div class="skillElementAdmin">
-            <p className="eachSkillElementAdmin">Guitar</p>
-            <img src={XLetter} alt="" className="xletterAdmin"/>
+        <div className="skillElementsContainer">
+        {projectSkills.map((skill) => {
+            return(
+                
+                <div class="skillElementAdmin">
+                    <p className="eachSkillElementAdmin">{skill}</p>
+                    <img src={XLetter} alt="" className="xletterAdmin"/>
+                </div>
+                    )
+                })}
         </div>
+
     </div>
     )
 }

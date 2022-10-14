e7d632d97f35104614fec887da6ded55da73eeb9
Ulrik Lunde <ulrik_lunde@hotmail.com>
2022-10-14 08:48:45 +0200

    feat: attributes change to using id

--- src/api/attributes.js
+++ src/api/attributes.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
+
 const apiUrl = "https://lagalt-java-backend.herokuapp.com/api/v1"
 
 //add industryId after async -> ...async (industryId)...
 //Change /industry/1/keywords -> /industry/${industryId}/keywords
-export const getKeyWordsOfIndustry = async () => {
+export const getKeyWordsOfIndustry = async (industry) => {
+    const id = getId(industry);
     try{
-        const response = await fetch(`${apiUrl}/industries/1/keywords`);
+        console.log("Logging ID: " + id);
+        const response = await fetch(`${apiUrl}/industries/${id}/keywords`);
         if(!response.ok){
             throw new Error("No keywords found");
         }
@@ -17,9 +21,11 @@ export const getKeyWordsOfIndustry = async () => {
 }
 
 //Add id, same as the function above
-export const getSkillsOfIndustry = async () => {
+export const getSkillsOfIndustry = async (industry) => {
+    const id = getId(industry)
     try{
-        const response = await fetch(`${apiUrl}/industries/1/skill`);
+        console.log("Logging ID: " + id);
+        const response = await fetch(`${apiUrl}/industries/${id}/skill`);
         if(!response.ok){
             throw new Error("No skills found");
         }
@@ -28,5 +34,17 @@ export const getSkillsOfIndustry = async () => {
 
     }catch(error){
         return[error.message, []];
+    }
+}
+
+function getId(industry){
+    if(industry === "Musikk"){
+        return 1;
+    }else if(industry === "Film"){
+        return 2;
+    }else if(industry === "Spillutvikling"){
+        return 3
+    }else if(industry === "Webutvikling"){
+        return 4;
     }
 }

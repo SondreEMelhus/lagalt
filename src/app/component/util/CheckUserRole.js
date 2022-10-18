2539ad8cf576e8ca259a0dabb1ec64efe64a0560
Sondre Melhus <42571126+SondreEMelhus@users.noreply.github.com>
2022-10-18 09:19:40 +0200

    Fixed typo

--- src/app/component/util/CheckUserRole.js
+++ src/app/component/util/CheckUserRole.js
@@ -1,6 +1,7 @@
 export const checkUserRole = (user, projectContributers) => {
     for (let contributer of projectContributers) {
-        if ( contributer.username === user.usernamer ) {
+        if ( contributer.username === user.username ) {
+            console.log(contributer.role);
             return contributer.role;
         }
     }

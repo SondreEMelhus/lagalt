bb1356aeeee6b09a4b92cfde8a3b91baeeec6e22
Sondre Melhus <42571126+SondreEMelhus@users.noreply.github.com>
2022-10-21 15:23:29 +0200

    Reworked timestamp

--- src/app/component/util/TrimTimestamp.js
+++ src/app/component/util/TrimTimestamp.js
@@ -1,4 +1,23 @@
 export const trimTimestamp = (timestamp) => {
-    let formatedTimestamp = timestamp.substring(11,16) + '  ' + timestamp.substring(8,10) + '/' + timestamp.substring(5,7) + '-' + timestamp.substring(2,4);
-    return formatedTimestamp;
+
+    if (timestamp.length === 23) {
+        let hours = timestamp.substring(11,13).includes(':') ? '0' + timestamp.substring(11,12) : timestamp.substring(11,13);
+        let minutes = timestamp.substring(14,16).includes(':') ? '0' + timestamp.substring(13, 14) :  timestamp.substring(14,16);
+
+        return hours + ':' + minutes + '  ' + timestamp.substring(8,10) + '/' + timestamp.substring(5,7) + '-' + timestamp.substring(2,4);
+    }
+
+    if (timestamp.length === 24) {
+        let hours = timestamp.substring(11,13).includes(':') ? '0' + timestamp.substring(11,12) : timestamp.substring(11,13);
+        let minutes = timestamp.substring(14,16).includes(':') ? '0' + timestamp.substring(14, 15) :  timestamp.substring(14,16);
+
+        return hours + ':' + minutes + '  ' + timestamp.substring(8,10) + '/' + timestamp.substring(5,7) + '-' + timestamp.substring(2,4);
+    }
+
+    if (timestamp.length === 25) {
+        let hours = timestamp.substring(11,13);
+        let minutes = timestamp.substring(14,16);
+
+        return hours + ':' + minutes + '  ' + timestamp.substring(8,10) + '/' + timestamp.substring(5,7) + '-' + timestamp.substring(2,4);
+    }
 }
